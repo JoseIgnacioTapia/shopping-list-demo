4bd66a8ed2eba8ef605d0a35939367fd4c18ac5e
JoeIt <josignaciot@gmail.com>
2023-05-01 19:51:30 -0300

    useState with Typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
+import { useState } from 'react';
 import './App.css';
+import { Item } from './models/item';
 import ShoppingList from './components/ShoppingList';
 
 function App() {
-  const items = [
-    { id: 1, product: 'Lemon', quantity: 3 },
-    { id: 2, product: 'Chicken Breast', quantity: 3 },
-  ];
+  const [items, setItems] = useState<Item[]>([]);
+
+  // const items = [
+  //   { id: 1, product: 'Lemon', quantity: 3 },
+  //   { id: 2, product: 'Chicken Breast', quantity: 3 },
+  // ];
 
   return (
     <div>
