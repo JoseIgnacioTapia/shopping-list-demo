f8dbb8fa1d7668da9dbd5db3a5d41e8f7d0403b9
JoeIt <josignaciot@gmail.com>
2023-05-01 20:48:16 -0300

    useRef With TypeScript

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,15 @@ import { useState } from 'react';
 import './App.css';
 import { Item } from './models/item';
 import ShoppingList from './components/ShoppingList';
+import ShoppingListForm from './components/ShoppingListForm';
 
 function App() {
   const [items, setItems] = useState<Item[]>([]);
+
+  const addItem = (product: string) => {
+    console.log('MADE TO THE APP COMPONENT!');
+    console.log(product);
+  };
 
   // const items = [
   //   { id: 1, product: 'Lemon', quantity: 3 },
@@ -14,6 +20,7 @@ function App() {
   return (
     <div>
       <ShoppingList items={items} />
+      <ShoppingListForm onAddItem={addItem} />
     </div>
   );
 }
