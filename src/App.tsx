8543c4d99cc4adc83ccfe96b565756de70468868
JoeIt <josignaciot@gmail.com>
2023-05-01 21:00:41 -0300

    Updating The State

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,14 @@ import './App.css';
 import { Item } from './models/item';
 import ShoppingList from './components/ShoppingList';
 import ShoppingListForm from './components/ShoppingListForm';
+import { v4 as getId } from 'uuid';
 
 function App() {
   const [items, setItems] = useState<Item[]>([]);
 
   const addItem = (product: string) => {
     console.log('MADE TO THE APP COMPONENT!');
-    console.log(product);
+    setItems([...items, { id: getId(), product, quantity: 1 }]);
   };
 
   // const items = [
